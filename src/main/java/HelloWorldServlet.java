ae4d47aa92a3a075a2fa52eb5810d19b7b1fd819
Fernando Mendoza <fernandoza@gmail.com>
2019-10-22 10:31:01 -0500

    feat: connect the Controller with the view

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,24 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World!";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -8,6 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%
 
+
     String username = (request.getParameter("username") != null) ? request.getParameter("username") : "";
     String password = (request.getParameter("password") != null) ? request.getParameter("password") : "";
 
